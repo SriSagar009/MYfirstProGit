9f9adfc696331744f489166fa9a2c8ebd3618d67
SriSagar009 <168548341+SriSagar009@users.noreply.github.com>
2024-05-04 15:57:33 +0530

    logins test created

---
+++ src/SeleniumTestNg/Loginssstett.java
@@ -0,0 +1,14 @@
+package SeleniumTestNg;
+
+import org.testng.annotations.Test;
+
+public class Loginssstett {
+	
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("hello worldl ");
+	}
+
+}
